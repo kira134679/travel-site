86ccd0235a1855ec94cb58ad8650ae19da08e4a8
kira134679 <kira134679@gmail.com>
2025-11-16 23:46:40 +0800

    chore: update Prettier and Nuxt configuration for Tailwind CSS integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devServer: {
+    port: 5678,
+  },
 });
